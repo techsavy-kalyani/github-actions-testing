56029e34a5fd59cc3974e95d4c556f224751d55f
KalyaniDas <kalyani.das@planview.com>
2022-02-07 11:24:27 +0530

    update run_sales_tests.yml

--- automation/tests/functional/sales/divisible_tests.py
+++ automation/tests/functional/sales/divisible_tests.py
@@ -25,3 +25,8 @@ def test_divisible_by_3(input_value):
 @pytest.mark.divisible
 def test_divisible_by_13(input_value):
     assert input_value % 13 == 0
+
+
+@pytest.mark.parametrize("num, output", [(1, 11), (2, 22), (3, 35), (4, 44)])
+def test_multiplication_11(num, output):
+    assert 11 * num == output
