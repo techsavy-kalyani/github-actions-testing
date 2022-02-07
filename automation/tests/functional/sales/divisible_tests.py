4520c035cff6def54ba12a94c4e30f4b24ee876b
KalyaniDas <kalyani.das@planview.com>
2022-02-07 10:10:32 +0530

    creating directory in poweshell and install project

---
+++ automation/tests/functional/sales/divisible_tests.py
@@ -0,0 +1,27 @@
+import pytest
+
+
+@pytest.fixture
+def input_value():
+    input = 39
+    return input
+
+
+def list_input_value():
+    my_list = [12, 65, 54, 39, 102, 339, 221]
+    return my_list
+
+
+@pytest.mark.divisible
+def test_divisible_by_2(input_value):
+    assert input_value % 2 == 0
+
+
+@pytest.mark.divisible
+def test_divisible_by_3(input_value):
+    assert input_value % 3 == 0
+
+
+@pytest.mark.divisible
+def test_divisible_by_13(input_value):
+    assert input_value % 13 == 0
